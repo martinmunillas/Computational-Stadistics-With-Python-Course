f488c1848702febe12f8c4d185b42a27d7dce7e7
Martin Munilla <martinmunillas@gmail.com>
2021-02-01 16:12:21 -0300

    PI

--- montecarlo/pi.py
+++ montecarlo/pi.py
@@ -1,14 +1,49 @@
 import random
 
-def throw_needle(tries):
+def average(x):
+    return sum(x) / len(x)
+
+def variance(X):
+    mu = average(X)
+    acc = 0
+    for x in X:
+        acc += (x - mu)**2
+    return acc / len(X)
+
+def standard_deviation(X):
+    return variance(X)**0.5
+
+def throw_needles(needles):
     inside = 0
 
 
-    for _ in range(tries):
-        x = random.random() * random.randint(-1,1)
-        y = random.random() * random.randint(-1,1)
-        distance = (x**2=y**2)**0.5
+    for _ in range(needles):
+        x = random.random() * random.choice([-1,1])
+        y = random.random() * random.choice([-1,1])
+        distance = (x**2+y**2)**0.5
 
-        if distance >= 1:
+        if distance <= 1:
             inside +=1
-    return (4 * inside)/ tries
+    return (4 * inside)/ needles
+
+def estimate_pi(needles, tries):
+    pis = []
+
+    for _ in range(tries):
+        pis.append(throw_needles(needles))
+    
+    average_pi = average(pis)
+    sigma = standard_deviation(pis)
+    print(f'Average: {average_pi}, sigma: {sigma}, needles={needles}')
+    return (average_pi, sigma)
+    
+def pi(precision, tries):
+    needles = 1000
+    sigma = precision
+
+    while sigma >= precision / 1.96:
+        average, sigma = estimate_pi(needles, tries)
+        needles *= 2
+    return average
+if __name__ == '__main__':
+    pi(0.01, 100)
